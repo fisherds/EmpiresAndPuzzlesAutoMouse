817c5074b3a96c01d02015d1a6e469dfc9bb8ae1
David Fisher <fisherds@rose-hulman.edu>
2021-01-16 23:45:46 -0500

    improved device support

--- src/continuous_level_replay.py
+++ src/continuous_level_replay.py
@@ -1,8 +1,25 @@
 # https://pyautogui.readthedocs.io/en/latest/
-
+#uu
 import pyautogui
 import time
 
+import keyboard
+import sys
+
+
+
+MAX_TIME_M = 35
+
+DOING_POINT_FINDING = False
+USING_MOTO_X4 = True
+
+# Replay, Next, Auto
+MOTO_G7_POINTS = [(812, 846), (1146, 962), (1101, 117)]
+MOTO_X4_POINTS =  [(789, 884), (1190, 958), (1127, 66)]
+
+points = MOTO_G7_POINTS
+if USING_MOTO_X4:
+    points = MOTO_X4_POINTS
 
 screenWidth, screenHeight = pyautogui.size() # Get the size of the primary monitor.
 print(screenHeight)
@@ -10,12 +27,18 @@ print(screenWidth)
 start_time = time.time()
 while True:
     time.sleep(0.5)
-    # currentMouseX, currentMouseY = pyautogui.position() # Get the XY position of the mouse.
-    # print("({:2}, {:2}".format(currentMouseX, currentMouseY))
+    if DOING_POINT_FINDING:
+        currentMouseX, currentMouseY = pyautogui.position() # Get the XY position of the mouse.
+        print("({:2}, {:2})".format(currentMouseX, currentMouseY))
+        continue
 
-    pyautogui.moveTo(1532, 927, duration=.25, tween=pyautogui.easeInOutQuad)
+    if keyboard.is_pressed(' '):
+        print('Exit')
+        sys.exit()
+
+    pyautogui.moveTo(points[0][0], points[0][1], duration=.25, tween=pyautogui.easeInOutQuad)
     pyautogui.click()
-    pyautogui.moveTo(1852, 986, duration=.25, tween=pyautogui.easeInOutQuad)
+    pyautogui.moveTo(points[1][0], points[1][1], duration=.25, tween=pyautogui.easeInOutQuad)
     pyautogui.click()
 
     # pyautogui.moveTo(1639, 678, duration=.25, tween=pyautogui.easeInOutQuad)
@@ -23,13 +46,12 @@ while True:
     # time.sleep(0.5)
     # pyautogui.click()
 
-
-    pyautogui.moveTo(1812, 98, duration=.25, tween=pyautogui.easeInOutQuad)
+    pyautogui.moveTo(points[2][0], points[2][1], duration=.25, tween=pyautogui.easeInOutQuad)
     pyautogui.click()
 
-    elapsed_time_min = (time.time() - start_time) / 40
+    elapsed_time_min = (time.time() - start_time) / 60
     print(elapsed_time_min)
-    if elapsed_time_min > 40:
+    if elapsed_time_min > MAX_TIME_M:
         break
 #
 # pyautogui.moveTo(100, 150) # Move the mouse to XY coordinates.
